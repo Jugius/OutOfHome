b46eeb965e4b0cbc47ed90c2a87164e098e675aa
Andrew Slusarev <jugius@gmail.com>
2020-09-26 19:27:24 +0300

    Add new helper projects with defaults cities

---
+++ OutOfHome.Helpers.Cities/RegionsDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OutOfHome.Helpers.Cities
+{
+    static class RegionsDb
+    {
+        public const string Vinnitskaya = "Винницкая";
+        public const string Volynskaya = "Волынская";
+        public const string Dnepropetrovskaya = "Днепропетровская";
+        public const string Donetskaya = "Донецкая";
+        public const string Zhitomirskaya = "Житомирская";
+        public const string Zakarpatskaya = "Закарпатская";
+        public const string Zaporozhskaya = "Запорожская";
+        public const string IvanoFrankovskaya = "Ивано-Франковская";
+        public const string Kievskaya = "Киевская";
+        public const string Kirovogradskaya = "Кировоградская";
+        public const string Luganskaya = "Луганская";
+        public const string Lvovskaya = "Львовская";
+        public const string Nikolaevskaya = "Николаевская";
+        public const string Odesskaya = "Одесская";
+        public const string Poltavskaya = "Полтавская";
+        public const string Rovenskaya = "Ровенская";
+        public const string Sumskaya = "Сумская";
+        public const string Ternopolskaya = "Тернопольская";
+        public const string Kharkovskaya = "Харьковская";
+        public const string Khersonskaya = "Херсонская";
+        public const string Khmelnitskaya = "Хмельницкая";
+        public const string Cherkasskaya = "Черкасская";
+        public const string Chernigovskaya = "Черниговская";
+        public const string Chernovitskaya = "Черновицкая";
+    }
+}
