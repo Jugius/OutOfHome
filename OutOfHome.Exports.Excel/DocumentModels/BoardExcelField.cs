799c95c34065adb0661b41309029b0b65c67f209
Andrew Slusarev <jugius@gmail.com>
2021-02-14 17:44:29 +0200

    сократить набор колонок экспорта бордов по умолчанию

--- OutOfHome.Exports.Excel/DocumentModels/BoardExcelField.cs
+++ OutOfHome.Exports.Excel/DocumentModels/BoardExcelField.cs
@@ -16,7 +16,6 @@ namespace OutOfHome.Exports.Excel.DocumentModels
         public static List<BoardExcelField> GetDefaultColumns() => new List<BoardExcelField>(20)
             {
                 new BoardExcelField(BoardProperty.Supplier),
-                new BoardExcelField(BoardProperty.ProviderID),
                 new BoardExcelField(BoardProperty.SupplierCode),
                 new BoardExcelField(BoardProperty.Region),
                 new BoardExcelField(BoardProperty.City),
@@ -27,8 +26,7 @@ namespace OutOfHome.Exports.Excel.DocumentModels
                 new BoardExcelField(BoardProperty.Light),
                 new BoardExcelField(BoardProperty.URL_Photo),
                 new BoardExcelField(BoardProperty.URL_Map),
-                new BoardExcelField(BoardProperty.URL_DoorsPhoto),
-                new BoardExcelField(BoardProperty.URL_DoorsMap),
+                new BoardExcelField(BoardProperty.URL_DoorsPhoto),                
                 new BoardExcelField(BoardProperty.URL_GoogleMapPoint),
                 new BoardExcelField(BoardProperty.Price),
                 new BoardExcelField(BoardProperty.DoorsId),
@@ -39,31 +37,14 @@ namespace OutOfHome.Exports.Excel.DocumentModels
 
         private static int GetDefaultColumnWidth(BoardProperty kind)
         {
-            switch(kind)
+            return kind switch
             {
-                case BoardProperty.ProviderID:
-                case BoardProperty.SupplierCode:
-                case BoardProperty.City:
-                case BoardProperty.OccSource:
-                    return 15;
-
-                case BoardProperty.Provider:
-                case BoardProperty.Supplier:
-                    return 17;
-
-                case BoardProperty.Address:
-                    return 60;
-
-                case BoardProperty.Side:
-                case BoardProperty.Size:
-                case BoardProperty.Light:
-                case BoardProperty.OTS:
-                case BoardProperty.GRP:
-                case BoardProperty.Color:
-                    return 7;
-
-                default: return 8;
-            }
+                BoardProperty.ProviderID or BoardProperty.SupplierCode or BoardProperty.City or BoardProperty.OccSource => 15,
+                BoardProperty.Provider or BoardProperty.Supplier => 17,
+                BoardProperty.Address => 60,
+                BoardProperty.Side or BoardProperty.Size or BoardProperty.Light or BoardProperty.OTS or BoardProperty.GRP or BoardProperty.Color => 7,
+                _ => 8,
+            };
         }
     }  
 }
